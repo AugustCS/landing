aeadf5761df639ffbcd0337e41dcdd335cb824b9
Carlos Cubas <cusan2010@gmail.com>
2018-04-01 21:47:55 -0500

    Subida de Archivos

---
+++ js/logica.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$(".type").typed({
+		strings: ["Arqui Design  - Kholer","Welcome!"],
+		stringsElement: null,
+		loop:true,
+		fadeOut:true,
+		startDelay:0,
+		typeSpeed:300,
+		backDelay: 0,
+		shuffle:true,
+		attr:null
+	});
+
+});
+
